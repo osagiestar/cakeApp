4d07f7c491e6b22b47fe396cd4f8fab644a5c7a4
Osagie <okoedooc@yahoo.com>
2021-04-16 13:08:17 +0100

    updated the data.js and paased GET request - checked on POSTMAN

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+   module.exports = {
+    data: [
+        {   id1 : 1,
+            image: './Cake_images/2year Birthday Cake.jpg',
+            name: "Toddler Birthday Cake",
+            price: 50
+        },
+        {   id1 : 2,
+            image: './Cake_images/4th Birthday Cake.jpg',
+            name: "Toddler Birthday Cake",
+            price: 60
+        },
+       {    id1 : 3,
+            image: './Cake_images/2year Birthday Cake.jpg',
+            name: "Toddler Birthday Cake",
+            price: 50
+        },
+        {   id1 : 4,
+            image: './Cake_images/4th Birthday Cake.jpg',
+            name: "Toddler Birthday Cake",
+            price: 60
+        },
+        {   id1 : 5,
+            image: './Cake_images/2year Birthday Cake.jpg',
+            name: "Toddler Birthday Cake",
+            price: 50
+        },
+        {   id1 : 6,
+            image: './Cake_images/4th Birthday Cake.jpg',
+            name: "Toddler Birthday Cake",
+            price: 60
+        }
+    ]
+   }
+ 
+
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-
-app.get("/cakes", function (req, res) {
-  res.send(item);
+const detail = require('./data.js')
+ 
+app.get("/cake", function (req, res) {
+  // res.send("Hello World!");
+  console.log(detail.data[0])
+  // res.send(detail.data[0]);
+res.send(detail.data[0])
 });
-
-
 
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
