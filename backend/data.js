a54ae59df67e1d028c42a8e4ab194c5c1e4bef1d
Osagie <okoedooc@yahoo.com>
2021-04-16 13:20:43 +0100

    amended the data.js

--- backend/data.js
+++ backend/data.js
@@ -1,31 +1,31 @@
    module.exports = {
     data: [
-        {   id1 : 1,
+        {   id : 1,
             image: './Cake_images/2year Birthday Cake.jpg',
             name: "Toddler Birthday Cake",
             price: 50
         },
-        {   id1 : 2,
+        {   id : 2,
             image: './Cake_images/4th Birthday Cake.jpg',
             name: "Toddler Birthday Cake",
             price: 60
         },
-       {    id1 : 3,
+       {    id : 3,
             image: './Cake_images/2year Birthday Cake.jpg',
             name: "Toddler Birthday Cake",
             price: 50
         },
-        {   id1 : 4,
+        {   id : 4,
             image: './Cake_images/4th Birthday Cake.jpg',
             name: "Toddler Birthday Cake",
             price: 60
         },
-        {   id1 : 5,
+        {   id : 5,
             image: './Cake_images/2year Birthday Cake.jpg',
             name: "Toddler Birthday Cake",
             price: 50
         },
-        {   id1 : 6,
+        {   id : 6,
             image: './Cake_images/4th Birthday Cake.jpg',
             name: "Toddler Birthday Cake",
             price: 60
