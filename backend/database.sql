5f73616b5997e5cd68748cb92b2de4f9dc1c8f99
Osagie <okoedooc@yahoo.com>
2021-05-13 01:53:45 +0100

    added users table and inserted data in database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,10 +1,18 @@
-CREATE DATABASE cakedb;
+-- CREATE DATABASE cakedb;
+drop table if exists cakes;
+drop table if exists customers;
 
 CREATE TABLE cakes (
     id SERIAL PRIMARY KEY,
     image VARCHAR(100) 
     name VARCHAR(30) NOT NULL,
     price INT NOT NULL
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(30),
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE customers (
@@ -21,11 +29,11 @@ CREATE TABLE customers (
 INSERT INTO cakes(name, price) VALUES('', '2year Birthday Cake', 50);
 INSERT INTO cakes(name, price) VALUES('4th Birthday Cake', 60);
 
-INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('John Smith','j.smith@johnsmith.org','11 New Road','Liverpool','L10 2AB','UK');
-INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Sue Jones','s.jones1234@gmail.com','120 Old Street','London','N10 3CD','UK');
-INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Alice Evans','alice.evans001@hotmail.com','3 High Road','Manchester','m13 4ef','UK');
-INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Mohammed Trungpa','mo.trungpa@hotmail.com','25 Blue Road','Manchester','M25 6GH','UK');
-INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Steven King','steve.king123@hotmail.com','19 Bed Street','Newtown', 'xy2 3ac','UK');
+INSERT INTO users (type, name) VALUES('Admin', 'Jesse O');
+INSERT INTO users (type, name) VALUES('Admin', 'Jerry O');
+INSERT INTO users (type, name) VALUES('Support', 'Garry S');
+INSERT INTO users (type, name) VALUES('Clerk', 'Jane K');
+
 INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Nadia Sethuraman','nadia.sethuraman@mail.com','135 Green Street','Manchester','M10 4BG','UK');
 INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Melinda Marsh','mel.marsh-123@gmail.com','7 Preston Road','Oldham','OL3 5XZ','UK');
 INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('Martín Sommer','martin.sommer@dfgg.net','C/ Romero, 33','Madrid','28016','Spain');
