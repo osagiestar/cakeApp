2be51e670e532b91a140939f84ae72119853693e
Osagie <okoedooc@yahoo.com>
2021-07-13 04:15:39 +0100

    resolve Customer and HomePage Component conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import "./App.css";
 // import data from './data';
 import { BrowserRouter } from 'react-router-dom';
-// import HomePage from './Components/HomePage';
+import HomePage from './Components/HomePage';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Customer from './Components/Customer'; 
 
-function App() {
+function App(props) {
   return (
-     
     <BrowserRouter>
       <div className="App">
         <div className="grid-container">
           <Header />
-          {/* <HomePage /> */}
-         <Footer year = {new Date().getFullYear()}/>
+          {!props.customerList ? <HomePage /> : <Customer />}
+          <Footer year={new Date().getFullYear()} />
         </div>
       </div>
     </BrowserRouter>
