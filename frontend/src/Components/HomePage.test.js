9389167789fffd5f4951c7c109179724a9e69b2d
Osagie Okoedo <okoedooc@yahoo.com>
2021-08-24 09:32:23 +0100

    testing the HomePage component

--- frontend/src/Components/HomePage.test.js
+++ frontend/src/Components/HomePage.test.js
@@ -4,3 +4,20 @@ const dataLength = data.length;
 test("total number of cake images to equal 6", () => {
   expect(dataLength).toEqual(6);
 });
+
+
+const allCakeIds = data.map(item => item.id); 
+test("first cake has a property id as numbers", () => {
+    expect(allCakeIds).toEqual([...allCakeIds])
+})
+
+const firstCake = data[0].id;
+test("first cake has a property id value of 1", () => {
+    expect(firstCake).toBe(1)
+})
+
+
+test("all cakes have the property image", () => {
+    const allCakesHasAnImage = data.map(item => item.image); 
+    expect(allCakesHasAnImage).toEqual([...allCakesHasAnImage])
+})
