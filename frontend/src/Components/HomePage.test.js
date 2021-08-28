6cd406f050122d6b7df221f27aa950804562bba9
Osagie Okoedo <okoedooc@yahoo.com>
2021-08-28 04:22:49 +0100

    testing and more testing

--- frontend/src/Components/HomePage.test.js
+++ frontend/src/Components/HomePage.test.js
@@ -5,19 +5,29 @@ test("total number of cake images to equal 6", () => {
   expect(dataLength).toEqual(6);
 });
 
-
 const allCakeIds = data.map(item => item.id); 
 test("first cake has a property id as numbers", () => {
     expect(allCakeIds).toEqual([...allCakeIds])
 })
 
-const firstCake = data[0].id;
+const firstCakeId = data[0].id;
 test("first cake has a property id value of 1", () => {
-    expect(firstCake).toBe(1)
+    expect(firstCakeId).toBe(1)
+})
+
+// testing all cakes
+describe("all cake tests", () => {
+    test("all cakes have the property image", () => {
+        const allCakesHasAnImage = data.map(item => item.image); 
+        expect(allCakesHasAnImage).toEqual([...allCakesHasAnImage])
+    })
+    
+test("property of cakes", () => {
+    for(let i = 0; i < data.length; i++) {
+    expect(data[i]).toHaveProperty("id", "image", "name", "price")
+}
+})
 })
 
 
-test("all cakes have the property image", () => {
-    const allCakesHasAnImage = data.map(item => item.image); 
-    expect(allCakesHasAnImage).toEqual([...allCakesHasAnImage])
-})
+
